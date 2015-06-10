a382e05d5ab37e630f0888ca1ea1f60f7e934f40
Alexander Kozlov <akozlov@nada.kth.se>
2015-06-10 22:44:00 +0200

    underwater

--- work/grf4/demo4.py
+++ work/grf4/demo4.py
@@ -4,5 +4,6 @@ from apps import App
 import physics
 import scene
 
-app = App("demo", scene.PoolBottom(), physics.DrivenLamprey())
+#app = App("demo", scene.PoolBottom(), physics.DrivenLamprey())
+app = App("demo", scene.Underwater(), physics.DrivenLamprey())
 app.run()
--- work/grf4/scene.py
+++ work/grf4/scene.py
@@ -1,6 +1,6 @@
 from OpenGL.GL import *
-#from OpenGL.GLU import *
-#from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
 
 from tools import load_texture
 
@@ -21,7 +21,7 @@ class PoolBottom:
     def __init__(self, level=0.0):
         self.made = False
         self.level = level
-        self.ground = None
+        self.underwater = None
 
     def draw(self):
         if not self.made:
@@ -59,3 +59,31 @@ class PoolBottom:
         glTranslate(0.0, self.level, 0.0)
         glCallList(self.ground)
         glPopMatrix()
+
+class Underwater:
+    def __init__(self, level=0.0):
+        self.made = False
+	self.underwater = None
+
+    def draw(self):
+	if not self.made:
+	    glMaterialfv(GL_FRONT, GL_EMISSION, (0.3, 0.3, 0.3, 1))
+	    glEnable(GL_TEXTURE_2D)
+	    quadric = gluNewQuadric()
+	    gluQuadricNormals(quadric, GLU_SMOOTH)
+	    gluQuadricTexture(quadric, 1)
+	    texture = load_texture("images/underwater2.jpg")
+	    self.underwater = glGenLists(1)
+	    glNewList(self.underwater, GL_COMPILE)
+	    glBindTexture(GL_TEXTURE_2D, texture)
+	    glPushMatrix()
+	    glRotate(90, 1, 0, 0)
+	    gluSphere(quadric, 50.0, 15, 15)
+	    glPopMatrix()
+	    glBindTexture(GL_TEXTURE_2D, 0)
+	    glEndList()
+	    self.made = True
+        glPushMatrix()
+	glCallList(self.underwater)
+        glPopMatrix()
+
