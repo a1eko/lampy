ff4ecb74195c17b381df24b7a0c3f036c244db14
Alexander Kozlov <akozlov@nada.kth.se>
2015-06-22 13:59:25 +0200

    shape

--- demo4.py
+++ demo4.py
@@ -4,6 +4,6 @@ from apps import App
 import physics
 import scene
 
-#app = App("demo", scene.PoolBottom(), physics.DrivenLamprey())
-app = App("demo", scene.Underwater(), physics.DrivenLamprey())
+#app = App("demo", scene.Underwater(), physics.DrivenLamprey())
+app = App("demo", physics=physics.DrivenLamprey())
 app.run()
